d2f2e53b395c1afa76168dbe5579c9cf72f05eb3
mrvillage <mr.village05@gmail.com>
2021-09-24 16:45:22 -0400

    Update from ABOUT ME to WHAT IS RIFT?

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -54,7 +54,7 @@
       <div class="py-12"></div>
 
       <v-container class="text-center">
-        <h2 class="display-2 font-weight-bold mb-3">ABOUT ME</h2>
+        <h2 class="display-2 font-weight-bold mb-3">WHAT IS RIFT?</h2>
 
         <v-responsive class="mx-auto mb-8" width="56">
           <v-divider class="mb-1"></v-divider>
