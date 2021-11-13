4bd09f6bfdbcd838070b0c0087d823756df034ad
mrvillage <mr.village05@gmail.com>
2021-11-12 21:53:07 -0500

    Add credentials permissions

--- src/views/MyDashboardViews/General.vue
+++ src/views/MyDashboardViews/General.vue
@@ -4,32 +4,57 @@
       <v-card class="pa-5">
         <h2 class="pb-5">Set your credentials.</h2>
         <v-text-field
-          @change="saved = false"
+          @change="credentialsSaved = false"
           label="Your P&W API Key"
           v-model="apiKey"
           outlined
         />
         <v-text-field
-          @change="saved = false"
+          @change="credentialsSaved = false"
           label="Your P&W Username"
           v-model="username"
           outlined
         />
         <v-text-field
-          @change="saved = false"
+          @change="credentialsSaved = false"
           label="Your P&W Password"
           v-model="password"
           outlined
         />
-        <v-btn @click="save()" :disabled="saved" color="primary">Save</v-btn>
+        <v-btn
+          @click="saveCredentials()"
+          :disabled="credentialsSaved"
+          color="primary"
+          >Save</v-btn
+        >
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="6" v-if="userLinked">
+      <v-card class="pa-5">
+        <h2 class="pb-5">Set your credential permissions.</h2>
+        <v-switch
+          v-for="permission in permissionsValues"
+          :key="permission.value"
+          :label="permission.name"
+          :value="!!(credentialsPermissions & permission.value)"
+          dense
+          inset
+          @change="permissionsChange($event, permission.value)"
+        ></v-switch>
+        <v-btn
+          @click="saveCredentialsPermissions()"
+          :disabled="originalCredentialsPermissions == credentialsPermissions"
+          color="primary"
+          >Save</v-btn
+        >
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts">
-import { SupabaseClient } from "@supabase/supabase-js";
-import { Component, Vue } from "vue-property-decorator";
+import { SupabaseClient, User } from "@supabase/supabase-js";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import publicKey from "@/publicKey";
 // @ts-expect-error
 import NodeRSA from "node-rsa";
@@ -37,13 +62,16 @@ import { UserLink } from "@/types";
 
 const rsa = new NodeRSA(publicKey);
 rsa.setOptions({
-  // encryptionScheme: { schema: "pkcs1_oaep", hash: "sha256" },
   encryptionScheme: "pkcs1",
 });
 @Component
 export default class MyDashboardGeneral extends Vue {
   get supabase(): SupabaseClient {
     return this.$store.getters.supabase;
+  }
+
+  get user(): User | null {
+    return this.supabase.auth.user();
   }
 
   get userLinked(): boolean {
@@ -54,33 +82,140 @@ export default class MyDashboardGeneral extends Vue {
     return this.$store.getters.getUserLink;
   }
 
-  mounted() {}
+  async mounted() {
+    if (!this.userLink.nation_id) {
+      return;
+    }
+    const { data } = await this.supabase
+      .from("credentials")
+      .select("permissions")
+      .eq("nation", this.userLink.nation_id);
+    if (data?.length) {
+      this.credentialsPermissions = data[0].permissions;
+    } else {
+      this.credentialsPermissions = 0;
+    }
+    this.originalCredentialsPermissions = this.credentialsPermissions;
+  }
 
   apiKey: string = "";
   username: string = "";
   password: string = "";
-  saved = true;
+  credentialsSaved = true;
   rsa = rsa;
+  credentialsPermissions = 0;
+  originalCredentialsPermissions = 0;
+  permissionsValues = [
+    {
+      name: "Send Nation Bank",
+      value: 1 << 1,
+    },
+    {
+      name: "Send Alliance Bank",
+      value: 1 << 2,
+    },
+    {
+      name: "View Nation Bank",
+      value: 1 << 3,
+    },
+    {
+      name: "View Alliance Bank",
+      value: 1 << 4,
+    },
+    {
+      name: "Manage Alliance Treaties",
+      value: 1 << 5,
+    },
+    {
+      name: "Manage Alliance Positions",
+      value: 1 << 6,
+    },
+    {
+      name: "Manage Alliance Taxes",
+      value: 1 << 7,
+    },
+    {
+      name: "Manage Alliance Announcements",
+      value: 1 << 8,
+    },
+    {
+      name: "Manage Nation",
+      value: 1 << 9,
+    },
+    {
+      name: "Send Messages",
+      value: 1 << 10,
+    },
+    {
+      name: "Create Trade",
+      value: 1 << 11,
+    },
+    {
+      name: "Manage Trades",
+      value: 1 << 12,
+    },
+    {
+      name: "Declare War",
+      value: 1 << 13,
+    },
+    {
+      name: "Manage Wars",
+      value: 1 << 14,
+    },
+  ];
 
-  async save(): Promise<void> {
-    const insert_data: any = {};
+  async saveCredentials(): Promise<void> {
+    const insertData: any = {};
     if (this.apiKey) {
-      insert_data.api_key = this.rsa.encrypt(this.apiKey).toString("hex");
+      insertData.api_key = this.rsa.encrypt(this.apiKey).toString("hex");
     }
     if (this.username) {
-      insert_data.username = this.rsa.encrypt(this.username).toString("hex");
+      insertData.username = this.rsa.encrypt(this.username).toString("hex");
     }
     if (this.password) {
-      insert_data.password = this.rsa.encrypt(this.password).toString("hex");
+      insertData.password = this.rsa.encrypt(this.password).toString("hex");
     }
-    if (insert_data) {
+    if (insertData) {
       await this.supabase
         .from("credentials")
         .upsert(
-          { ...insert_data, nation: this.userLink.nation_id },
+          { ...insertData, nation: this.userLink.nation_id },
           { onConflict: "nation", returning: "minimal" }
         );
-      this.saved = true;
+      this.credentialsSaved = true;
+    }
+  }
+
+  async saveCredentialsPermissions(): Promise<void> {
+    await this.supabase.from("credentials").upsert(
+      {
+        permissions: this.credentialsPermissions,
+        nation: this.userLink.nation_id,
+      },
+      { onConflict: "nation", returning: "minimal" }
+    );
+    this.originalCredentialsPermissions = this.credentialsPermissions;
+  }
+
+  permissionsChange(event: boolean, value: number) {
+    this.credentialsPermissions = event
+      ? this.credentialsPermissions | value
+      : this.credentialsPermissions & ~value;
+  }
+
+  @Watch("userLink")
+  async onUserChange(): Promise<void> {
+    if (this.user) {
+      const { data } = await this.supabase
+        .from("credentials")
+        .select("permissions")
+        .eq("nation", this.userLink.nation_id);
+      if (data?.length) {
+        this.credentialsPermissions = data[0].permissions;
+      } else {
+        this.credentialsPermissions = 0;
+      }
+      this.originalCredentialsPermissions = this.credentialsPermissions;
     }
   }
 }
