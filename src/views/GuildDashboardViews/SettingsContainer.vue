b8343af2974243a4f863f636a02f078b7c6b56f2
bsnk-dev <61131051+bsnk-dev@users.noreply.github.com>
2021-12-27 22:14:12 -0600

    settings container for the settings ui

---
+++ src/views/GuildDashboardViews/SettingsContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card
+    full-width
+    height="60vh"
+    class="ml-2"
+  >
+    <v-card-title>
+      <v-btn
+        icon
+        large
+        @click="$router.push('/dashboard/server')"
+      >
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+
+      <v-divider
+        vertical
+        class="mr-6 ml-6"
+      />
+
+      <span class="font-weight-bold">
+        General Settings
+      </span>
+    </v-card-title>
+    <div class="ma-4">
+      <router-view/>
+    </div>
+  </v-card>
+</template>
