5cc929cd7a8f613e7f1e3a4582a10c4f6275f9a2
bsnk-dev <61131051+bsnk-dev@users.noreply.github.com>
2021-12-31 18:58:50 -0600

    basic settings scaffolding

--- src/views/GuildDashboardViews/Settings/General.vue
+++ src/views/GuildDashboardViews/Settings/General.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    Hello World
+    <h3>
+      Admin Settings
+    </h3>
+    <p>
+      High level settings for your server and dangerous options
+    </p>
+    <v-form class="mt-4">
+      <v-text-field
+        label="Server Purpose"
+        filled
+        :rules="[v => !!v || 'Server Name is required']"
+      >
+
+      </v-text-field>
+      <role-dropdown
+        multiple
+        label="Server Manager Roles"
+      />
+    </v-form>
   </div>
 </template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import RoleDropdown from '@/components/RoleDropdown.vue';
+
+@Component({
+  components: {
+    RoleDropdown,
+  },
+})
+export default class GeneralGuildDashboardSettingsView extends Vue {
+}
+</script>
