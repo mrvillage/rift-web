49c8848c8c0df0be4f622d1e12e489c160ded006
bsnk-dev <61131051+bsnk-dev@users.noreply.github.com>
2022-01-06 22:44:14 -0600

    updated ui for admin settings and server purpose argument thing

--- src/views/GuildDashboardViews/Settings/General.vue
+++ src/views/GuildDashboardViews/Settings/General.vue
@@ -1,24 +1,43 @@
 <template>
   <div>
-    <h3>
-      Admin Settings
-    </h3>
-    <p>
-      High level settings for your server and dangerous options
-    </p>
-    <v-form class="mt-4">
-      <v-text-field
-        label="Server Purpose"
-        filled
-        :rules="[v => !!v || 'Server Name is required']"
-      >
+    <section id="admin-settings">
+      <h2>
+        <a href="#admin-settings">Admin Settings</a>
+      </h2>
+      <p>
+        High level settings for your server and dangerous options
+      </p>
+      <v-form class="mt-4 form-border" style="max-width: 700px">
+        <p class="form-element-label">
+          Use this field to set the purpose of your server, this is displayed to people to let them know
+          more about what they're joining.
+        </p>
 
-      </v-text-field>
-      <role-dropdown
-        multiple
-        label="Server Manager Roles"
-      />
-    </v-form>
+        <v-text-field
+          label="Server Purpose"
+          filled
+        />
+
+        <p class="form-element-label">
+          Use this field to set the purpose of your server, this is displayed to people to let them know
+          more about what they're joining.
+        </p>
+
+        <v-text-field
+          label="Server Purpose Argument"
+          filled
+        />
+
+        <p class="form-element-label">
+          The roles you select below will be able to modify many of the server settings available here.
+        </p>
+
+        <role-dropdown
+          multiple
+          label="Server Manager Roles"
+        />
+      </v-form>
+    </section>
   </div>
 </template>
 
@@ -34,3 +53,23 @@ import RoleDropdown from '@/components/RoleDropdown.vue';
 export default class GeneralGuildDashboardSettingsView extends Vue {
 }
 </script>
+
+<style scoped>
+  .form-element-label {
+    font-size: 14px;
+    opacity: 0.8;
+    margin-bottom: 8px;
+  }
+
+  .form-border {
+    border: 1px solid #e0e0e067;
+    border-radius: 8px;
+    padding: 16px;
+    margin-left: 8px;
+  }
+
+  h2 a {
+    color: white;
+    text-decoration: none;
+  }
+</style>
